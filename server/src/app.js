ac5ad3364771edc858895d75142da33b8728e79a
Vimukthi karunarathne <vimukthikarunarathne02@gmail.com>
2026-08-31 20:23:36 +0530

    feat: implement Board & Column management, routes, and task moving

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const authRoutes = require("./routes/authRoutes");
+const boardRoutes = require("./routes/boardRoutes");
 const cors = require("cors");
 require("dotenv").config();
 
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/auth", authRoutes);
+app.use("/api/boards", boardRoutes);
 
 // ================================
 // API Health Check
